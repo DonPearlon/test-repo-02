74e9bdf6b458450cbe59b89845fc838d0f5b521c
Aleksandr Terehov <Aliaksandr_Tserakhau@epam.com>
2020-11-19 18:03:27 +0300

    Combined commit. Subtraction, division, multiplication, sqrt methods.

--- BaseClass.java
+++ BaseClass.java
@@ -2,4 +2,26 @@ public class BaseClass {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new NullPointerException();
+        }
+        return a / b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double sqrt(int a) {
+        if (a == 0) {
+            throw new NullPointerException();
+        }
+        return Math.sqrt(a);
+    }
 }
